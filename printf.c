5382315ac16c46d3e7175fc4b70843c8da658c0d
Israelkw <srlkbd@gmail.com>
2023-04-24 19:27:00 +0000

    again

--- printf.c
+++ printf.c
@@ -48,3 +48,25 @@ int _printf(const char *format, ...)
 	va_end(args);
 	return (count);
 }
+
+/**
+ * print_string - prints a string
+ * @s: the string to print
+ *
+ * Return: the number of characters printed
+ */
+int print_string(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+	s = "(null)";
+
+	while (s[i])
+	{
+	_putchar(s[i]);
+	i++;
+	}
+
+	return (i);
+}
